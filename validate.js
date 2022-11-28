3ad76f18542b9bb01e0edff7e03194baa51f19b3
abloodgood <114004494+abloodgood@users.noreply.github.com>
2022-11-28 14:37:06 -0500

    various updates

--- validate.js
+++ validate.js
@@ -5,11 +5,11 @@ function validateForm() {
 		/*Check to make sure first and last name are both over 2 characters*/
 		if (firstName.value.length <= 2) {
 					alert("First Name must contain two or more characters!");
-					firstName.focus();
+					firstName.style.borderColor = "red";
 					return false;
 		} else if (lastName.value.length <= 2) {
 				  alert("Last Name must contain two or more characters!");
-					lastName.focus();
+					lastName.style.borderColor = "red";
 					return false;
 		}
 		
@@ -18,11 +18,13 @@ function validateForm() {
 		if ((!letters.test(firstName.value)) 
 		|| (!letters.test(lastName.value))) {
 					alert("Names can only contain letters!");
+					firstName.style.borderColor = "red";
+					lastName.style.borderColor = "red";
 					return false;
 		}
 		
 		/*check that facilatator exists*/
-		var facName = document.getElementById("facilitatorName");
+		var facName = document.getElementById("facilitator");
 		var names = ["Jen", "Behdad", "Chris", "Christian", "Josh", "jen", "behdad", "chris", "christian", "josh"];
 		/*if the string exists in names, return true*/
 		if (names.indexOf(facName.value) > -1) {
@@ -30,6 +32,7 @@ function validateForm() {
 		/*print alert and return false if facilitator name does not exist*/
 		} else {
 				alert("No facilitator by that name was found. Please enter one of the following: Jen, Behdad, Chris, Christian, Josh");
+				facName.style.borderColor = "red";
 				return false;
 		}
 }
